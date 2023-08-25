7908ed9e4bc3c9dbcd1ac814365bb2493e809aeb
Masoumeh Rezai <you@example.com>
2023-08-25 21:46:45 +0430

    add post

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use((req, res, next) => {
-    console.log('this is awalyyes ');
-    next()
 
-})
-app.use('/add', (req, res, next) => {
-    console.log('this is the frist middle');
-    res.send('<form action="/product" method="POST"><input title="name" type="text"><button>Add</button></form>');
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
 
-})
-app.use('product', (req, res, next) => {
-    console.log(req.body)
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
     res.redirect('/');
-})
-app.use('/second', (req, res, next) => {
-    console.log("Hello my name is second middle");
-    res.send('<h1> Hello Mahsumeh to express js class</h1>')
-})
+});
 
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
 
-
-
-
-
-
-app.listen(4000)
-
-
+app.listen(4000);
