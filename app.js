043ec00b5d796209b30c2497c59fbd2da1d3569b
Masoumeh Rezai <you@example.com>
2023-08-25 09:39:27 +0430

    add express js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const http = require('http');
+const express=require('express');
+const app =express()
+const server = http.createServer(app)
 
 
-const server = http.createServer((req, res) => {
-    //console.log(res)
-    const url = req.url;
-    if (url === '/') {
-        res.write('<h1> Hi I am node js and are Masoumeh</h1>')
-
-        return res.end()
-    }
-    res.write('<h1> Hi I am node js</h1>')
-})
 server.listen(4000)
+
+
