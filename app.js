2f41499975ce0ba3c06d2bf6778ea013649bc732
Masoumeh Rezai <you@example.com>
2023-08-25 10:34:59 +0430

    add router

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 app.use((req, res, next) => {
-    console.log('this is the frist middle');
+    console.log('this is awalyyes ');
     next()
+
 })
-app.use((req, res, next) => {
+app.use('/add', (req, res, next) => {
+    console.log('this is the frist middle');
+    res.send('<form action="/product" method="POST"><input title="name" type="text"><button>Add</button></form>');
+
+
+})
+app.use('product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+app.use('/second', (req, res, next) => {
     console.log("Hello my name is second middle");
     res.send('<h1> Hello Mahsumeh to express js class</h1>')
 })
